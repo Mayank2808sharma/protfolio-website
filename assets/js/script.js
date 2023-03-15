e46d2325857b432f2d3112f2d3b7c0163dab539d
mayank sharma <93312881+Mayank2808sharma@users.noreply.github.com>
2023-03-15 08:54:49 +0530

    changing the wrting itself text

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,7 +74,7 @@ document.addEventListener("visibilitychange", function () {
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-  strings: ["frontend development", "web designing", "Machine Learning"],
+  strings: ["Frontend Development", "Blockchain Development", "Competitive Programming"],
   loop: true,
   typeSpeed: 50,
   backSpeed: 25,
